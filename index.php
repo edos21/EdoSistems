4db009a2be089da431d0bebe9f51d2d212ca678a
Wilmer Blanco <wilmervzla2000@gmail.com>
2013-08-30 00:15:11 -0430

    Update Index
    Fixed 3 box (services)
    Fixed Text (DISEÑAMOS Y DESARROLLAMOS TÚ PÁGINA WEB)
    Add background .jpg
    Working in footer (background an text) don't toch.
    Resize icon PNG (Desarrollo Asesorias Cursos)
    Add effects opacity in Social icon

--- index.php
+++ index.php
@@ -13,25 +13,72 @@
 					<a href="#"><img src="img/slider/3.jpg" alt="image03"><div></div></a>
 					<a href="#"><img src="img/slider/4.jpg" alt="image04"><div></div></a>
 					<a href="#"><img src="img/slider/5.jpg" alt="image05"><div></div></a>
-					<a href="#"><img src="img/slider/6.jpg" alt="image06"><div></div></a>
+				<!--	<a href="#"><img src="img/slider/6.jpg" alt="image06"><div></div></a>
 					<a href="#"><img src="img/slider/7.jpg" alt="image07"><div></div></a>
 					<a href="#"><img src="img/slider/8.jpg" alt="image08"><div></div></a>
 					<a href="#"><img src="img/slider/9.jpg" alt="image09"><div></div></a>
 					<a href="#"><img src="img/slider/10.jpg" alt="image10"><div></div></a>
 					<a href="#"><img src="img/slider/11.jpg" alt="image11"><div></div></a>
-					<a href="#"><img src="img/slider/12.jpg" alt="image12"><div></div></a>
+					<a href="#"><img src	="img/slider/12.jpg" alt="image12"><div></div></a> -->
 				</div>
-				<!--nav>	
-					<span class="dg-prev">&lt;</span>
-					<span class="dg-next">&gt;</span>
-				</nav-->
+				<br><br>
+				<div id="cont1">
+				<div id="textizq">DISEÑAMOS Y<br>DESARROLLAMOS</div>
+				<div id="textdere"> TÚ<br>PÁGINA <span style="font-size:50px;font-family:'robotoblack_italic';color: #0C7519;letter-spacing: 1px;">WEB</span></div>
+				</div>
 			</section>
+				<img id="sombra" src="img/shadow_1.png"/>
+	
 		</div>
-
 	<!--AQUI VA EL RESTO DE HOME-->
+		<br>
+		<br>
+		<div id="textcentral">¡Hola! Bienvenido a Edosistems, una empresa dedica al desarrollo y diseño Web, acá encontrarás toda la información referente a nuestros servicios que ponemos a tu disposición:</div>
+		<br>
+		<div id="textservicios">
+
+
+
+		</div>
+		
+		<br>
+<div id="cont2">
+
+	<div id="desarrollo">
+		<div id="logito"><img src="img/servi1.png"></div>
+		<br>
+		<b>Desarrollo</b>
+		<br>
+		¿Necesitas el desarrollo profesional de tu idea, proyecto, o bien un sitio web personal o corporativo? En Edosistems proyectamos excelencia, por ello te ofrecemos una amplia gama de servicios referentes al desarrollo desde front-end, back-end, desarrollo general, planificación del proyecto y más. Desarrollamos usando las últimas tecnologías y más productivas técnicas.
+		<br>
+
+	</div>
+
+	<div id="asesoria">
+		<div id="logito"><img src="img/servi2.png"></div>
+		<br>		
+		<b>Asesorias</b>
+		<br>
+		Dos cabezas piensas más que una. Ya seas profesional o estudiante, te ofrecemos personal capacitado para ayudarte en áreas de desarrollo y programación para cualquiera de tus proyectos, con atención individual o por grupos, con los más eficientes lenguajes y tecnologías y en excelentes precios y horarios diseñados para tu mayor comodidad.
+		<br>
+
+	</div>
+
+	<div id="cursos">
+		<div id="logito"><img src="img/servi3.png"></div>
+		<br>
+		<b>Cursos</b>
+		<br>
+		En Edosistems ofrecemos cursos prácticos y completos de las mejores tecnologías de Desarrollo Web, PHP, MySQL, JavaScript, HTML5 y más. 
+		<br>
+		Ponemos a tu disposición diferentes horarios y una flexible organización con los mejores precios. Pagos por hora o por curso completo, además de descuentos para grupos.
+		<br>
 
 	</div>
 	
+</div>
+
+</div>
 
 <?php
 
