b65c375c54b73c11339055a43daef074327ee908
Eduardo Sanchez <edos21@gmail.com>
2013-09-01 13:44:46 -0430

    update 01/09

--- section/about/index.php
+++ section/about/index.php
@@ -4,29 +4,36 @@
 
 ?>
 	<div><span class="letra">NOSOTROS</span></div>
-	<div id="nosotros">
+	<div id="texsec"> 
 		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce feugiat dictum scelerisque. Nulla vulputate eget neque ac placerat. Morbi at dignissim dolor. Nullam pretium accumsan nisi, vitae scelerisque nisi imperdiet quis. Vivamus elementum fermentum rhoncus. Nunc commodo urna id tellus hendrerit viverra. Nam non magna at leo feugiat sollicitudin. Pellentesque eu pellentesque lectus. Aenean tincidunt rutrum varius. Sed consectetur sapien nec nunc gravida varius. Proin tincidunt elit enim, id dictum arcu feugiat id.
 	</div>
 	<div id="textdere">Nuestro</div>
 	<div id="textizq"><span class="letra">EQUIPO</span>
 	</div>	
 	<div id="card-container">
-	   	<div class="flip" id="un">
+		
+	   	<div class="flip">
 	        <div class="back">This is the back of the card</div>
 	        <div class="front">This is the front of the card</div>
 	   	</div>
-	   	<div class="flip" id="dos">
+	   	
+		
+	   	<div class="flip">
 	        <div class="back">This is the back of the card</div>
 	        <div class="front">This is the front of the card</div>
-	   	</div>
-	   	<div class="flip" id="tres">
+	  	</div> 
+
+	   	<div class="flip">
 	        <div class="back">This is the back of the card</div>
 	        <div class="front">This is the front of the card</div>
-	   	</div>
-	   	<div class="flip" id="cuatro">
+	  	</div> 	
+
+	   	<div class="flip">
 	        <div class="back">This is the back of the card</div>
 	        <div class="front">This is the front of the card</div>
-	   	</div>
+	 	 </div> 	
+
+
 	</div>
 
 <?php
--- section/portfolio/index.php
+++ section/portfolio/index.php
@@ -2,9 +2,13 @@
 	
 	include '../../includes/header.html';
 
-?>
+?> 
 	
 	<div id="wrapper">
+		<div><span class="letra">PORTAFOLIO</span></div>
+		<div id="texsec">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce feugiat dictum scelerisque. Nulla vulputate eget neque ac placerat. Morbi at dignissim dolor. Nullam pretium accumsan nisi, vitae scelerisque nisi imperdiet quis. Vivamus elementum fermentum rhoncus. Nunc commodo urna id tellus hendrerit viverra. Nam non magna at leo feugiat sollicitudin. Pellentesque eu pellentesque lectus. Aenean tincidunt rutrum varius. Sed consectetur sapien nec nunc gravida varius. Proin tincidunt elit enim, id dictum arcu feugiat id.
+		</div>
 		<div id="contenedor3dimage">
 			<section id="dg-container" class="dg-container">
 				<div class="dg-wrapper">
