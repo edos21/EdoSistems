c4e505ded760150370fea453addf512c617e2678
Wilmer Blanco <wilmervzla2000@gmail.com>
2013-09-01 17:50:43 -0430

    Update page contact

--- section/contact/index.php
+++ section/contact/index.php
@@ -3,10 +3,38 @@
 	include '../../includes/header.html';
 
 ?>
+		<br>
+		<br>
+		<div id="textdere" style="text-align: right;">¿TIENES ALGUNA<br></div>
+				<div id="textizq"><span style="font-size:60px;font-family:'robotoblack_italic';color: #0C7519;letter-spacing: -2px;">PREGUNTA?</span>
+		</div>
+		<br>
+		<br>
+		<br>
+	<div id="contactbody">
+		<p>CONTACTANOS</p>
+		<br>
+			<div id="cont3">
+				<form>
+					NOMBRE Y APELLIDO: <input type="text" name="nombre"><br>
+					EMAIL: <input type="email" name="email"><br>
+					TELÉFONO: <input type="tel" name="telf"><br>
+					MENSAJE: <input style="height:300px;width:400px;"type="text" name="texto">
+				</form>
+				<div><img src="img/contact.png"></div>
+			</div>
+			<br>
+			<img src="img/enviar.png"/>
 	
-	<div id="wrapper">
-		
-		<h1>AQUI CONTACTO</h1>
+
+	<br>
+	<img src="img/separador.png"/><br>
+
+	<div id="textdere" style="text-align: right;">REDES<br></div>
+				<div id="textizq"><span style="font-size:60px;font-family:'robotoblack_italic';color: #0C7519;letter-spacing: -2px;">SOCIALES</span>
+	</div>
+	<br>
+	<img src="img/separador.png"/><br>
 
 	</div>
 
