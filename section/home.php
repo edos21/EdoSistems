f3181b26e97e368501fa6be44e1695b883250564
Eduardo Sanchez <edos21@gmail.com>
2013-09-08 17:19:41 -0430

    new slider at home

--- section/home.php
+++ section/home.php
@@ -7,21 +7,9 @@
 	<div id="wrapper">
 		<div id="contenedor3dimage">
 			<section id="dg-container" class="dg-container">
-				<div class="dg-wrapper">
-					<a href="#"><img src="img/slider/1.jpg" alt="image01"><div></div></a>
-					<a href="#"><img src="img/slider/2.jpg" alt="image02"><div></div></a>
-					<a href="#"><img src="img/slider/3.jpg" alt="image03"><div></div></a>
-					<a href="#"><img src="img/slider/4.jpg" alt="image04"><div></div></a>
-					<a href="#"><img src="img/slider/5.jpg" alt="image05"><div></div></a>
-				<!--	<a href="#"><img src="img/slider/6.jpg" alt="image06"><div></div></a>
-					<a href="#"><img src="img/slider/7.jpg" alt="image07"><div></div></a>
-					<a href="#"><img src="img/slider/8.jpg" alt="image08"><div></div></a>
-					<a href="#"><img src="img/slider/9.jpg" alt="image09"><div></div></a>
-					<a href="#"><img src="img/slider/10.jpg" alt="image10"><div></div></a>
-					<a href="#"><img src="img/slider/11.jpg" alt="image11"><div></div></a>
-					<a href="#"><img src	="img/slider/12.jpg" alt="image12"><div></div></a> -->
-				</div>
-				<br><br>
+
+				<iframe width="800px" height="350px" src="includes/slide.html" scrolling="no"></iframe>
+				<br>
 				<div class="aumento" id="cont1">
 				<div id="textizq">DISEÑAMOS Y<br>DESARROLLAMOS</div>
 				<div id="textdere"> TÚ<br>PÁGINA <span style="font-size:50px;font-family:'robotoblack_italic';color: #0C7519;letter-spacing: 1px;">WEB</span></div>
