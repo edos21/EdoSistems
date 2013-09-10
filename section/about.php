26225f7f769bc9cea99301a73fa36b745160f75b
Wilmer Blanco <wilmervzla2000@gmail.com>
2013-09-09 22:07:13 -0430

    Add circle efects.

--- section/about.php
+++ section/about.php
@@ -3,38 +3,66 @@
 	include 'includes/header.html';
 
 ?>
-	<div><span class="letra">NOSOTROS</span></div>
-	<div id="texsec"> 
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce feugiat dictum scelerisque. Nulla vulputate eget neque ac placerat. Morbi at dignissim dolor. Nullam pretium accumsan nisi, vitae scelerisque nisi imperdiet quis. Vivamus elementum fermentum rhoncus. Nunc commodo urna id tellus hendrerit viverra. Nam non magna at leo feugiat sollicitudin. Pellentesque eu pellentesque lectus. Aenean tincidunt rutrum varius. Sed consectetur sapien nec nunc gravida varius. Proin tincidunt elit enim, id dictum arcu feugiat id.
-	</div>
-	<div id="textdere">Nuestro</div>
-	<div id="textizq"><span class="letra">EQUIPO</span>
-	</div>	
-	<div id="card-container">
-		
-	   	<div class="flip">
-	        <img src="img/wil.jpg" class="front">
-	        <div class="back">This is the back of the card</div>
-	   	</div>
-	   	
-		
-	   	<div class="flip">
-	   		<img src="img/wil.jpg" class="front">
-	        <div class="back">This is the back of the card</div>
-	  	</div> 
+<br><br><br><br><br><br><br>
+<div class="wrapper">
+				<ul class="stage clearfix">
 
-	   	<div class="flip">
-	    <img src="img/wil.jpg" class="front">
-	        <div class="back">This is the back of the card</div>
-	  	</div> 	
+					<li class="scene">
+						<div class="movie" onclick="return true">
+							<div class="poster"></div>
+							<div class="info">
+								<header>
+									<h1>It's a Wonderful Life</h1>
+									<span class="year">1946</span>
+									<span class="rating">PG</span>
+									<span class="duration">130 minutes</span>
+								</header>
+								<p>
+									In Bedford Falls, New York on Christmas Eve, George Bailey is deeply troubled. Prayers for his well-being from friends and family reach Heaven. Clarence Odbody, Angel Second Class, is assigned to visit Earth to save George, thereby earning his wings. Franklin and Joseph, the head angels, review George's life with Clarence.
+								</p>
+							</div>
+						</div>
+					</li>
 
-	   	<div class="flip">
-	   		<img src="img/wil.jpg" class="front">
-	        <div class="back">This is the back of the card</div>
-	 	 </div> 	
+					<!-- <li class="scene">
+						<div class="movie" onclick="return true">
+							<div class="poster"></div>
+							<div class="info">
+								<header>
+									<h1>Vengeance Valley</h1>
+									<span class="year">1951</span>
+									<span class="rating">NR</span>
+									<span class="duration">83 minutes</span>
+								</header>
+								<p>
+									A cattle baron takes in an orphaned boy and raises him, causing his own son to resent the boy. As they get older the resentment festers into hatred, and eventually the real son frames his stepbrother for fathering an illegitimate child that is actually his, seeing it as an opportunity to get his half-brother out of the way so he can have his father's empire all to himself.
+								</p>
+							</div>
+						</div>
+					</li>
+
+					<li class="scene">
+						<div class="movie" onclick="return true">
+							<div class="poster"></div>
+							<div class="info">
+								<header>
+									<h1>The Gold Rush</h1>
+									<span class="year">1925</span>
+									<span class="rating">NR</span>
+									<span class="duration">95 minutes</span>
+								</header>
+								<p>
+									The Tramp travels to the Yukon to take part in the Klondike Gold Rush. He gets mixed up with some burly characters and falls in love with the beautiful Georgia. He tries to win her heart with his singular charm.
+								</p>
+							</div>
+						</div>
+					</li> -->
+
+				</ul>
+			</div><!-- /wrapper -->	
 
 
-	</div>
+
 
 <?php
 
