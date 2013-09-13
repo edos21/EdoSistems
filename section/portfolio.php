a7a8987e2dd41790878b42a6c267b15f12a76d58
Wilmer Blanco <wilmervzla2000@gmail.com>
2013-09-12 22:53:43 -0430

    Update portafolio

--- section/portfolio.php
+++ section/portfolio.php
@@ -29,6 +29,11 @@
 				</div>
 			</section>
 		</div>
+		<br>
+		<br>
+			<div id="textcentral">
+			Por este motivo nuestra empresa provee servicios de consultoría para tí, tu negocio o tu empresa, evaluando tus necesidades para así diseñar sitios web y desarrollar programas especialmente creados para impulsarte y ayudarte a crecer en el mundo de hoy, el mundo de la tecnología.
+			</div><br>
 	<img src="img/separador.png"/>
 
 	<div id="contenedor2"></div>
@@ -37,14 +42,145 @@
 			<div id="contpageimg">
 				<img src="img/page/1.png">
 			</div>
-
-			<div="contpagetext">
+			<div id="contpagetext">
+				<h3>ITBC C.A.</h3>
 				En este proyecto se planea crear una comunidad de usuarios en donde se suban libros de programación y desarrollo de forma gratuita, la idea original es motivar a los usuarios para que mas adelante ellos también compartan sus conocimientos, no solo de programación sino de cualquier área, pero claro empezando por programacion y las areas que nosotros conocemos, para no tener que depender solo de las colaboraciones de usuarios.
 			</div>
 
 		</section>
+		<br>
+		<section id="contpage">
 
+			<div id="contpageimg">
+				<img src="img/page/2.png">
+			</div>
+			<div id="contpagetext">
+				<h3>Colposalud</h3>
+				Bueno este sistema es mas o menos grande, pero yo solo colabore con el desarrollo del sistema interno y corrección de errores, el diseño no es mio (es horrible o.O) pero el sistema es registro de pacientes y de doctores, se realizaran citas, se enviaran resultados a pacientes, ademas se hacen exámenes colposcopicos (wilmer, deberías hacerte uno, a ver si tienes arena xD) cuenta con un sistema donde segun la imagen que se suba, se mostrara la zona afectada y también se puede crear la historia del paciente, cada paciente por supuesto puede tener varios exámenes, ademas los datos de los pacientes son totalmente confidenciales, por tanto cada doctor tiene acceso a solo sus pacientes y cada paciente tiene acceso a solo sus exámenes.
+			</div>
 
+		</section>
+		<br>
+		<section id="contpage">
+
+			<div id="contpageimg">
+				<img src="img/page/3.png">
+			</div>
+			<div id="contpagetext">
+				<h3>Admin</h3>
+				Es un sistemita administrativo solo pa mi pues :P donde puedo registrar clientes o cualquier persona para tenerlos como agenda, ademas puedo registrar mis gastos o ingresos, si son de alguna persona ya registrada puedo asociarlos y sino, solo se registra y fin. y me muestra un reporte con mis ganancias, gastos y mi balance segun cualquier rango de fecha que yo necesite :P.... chiquito, sencillo pero efectivo.
+			</div>
+
+		</section>
+		<br>
+		<section id="contpage">
+
+			<div id="contpageimg">
+				<img src="img/page/4.png">
+			</div>
+			<div id="contpagetext">
+				<h3>SERBAR</h3>
+				Sitio web, con información de la empresa, noticias, galería y contactos, y un sistema que abarca diversas tareas relacionadas al área laboral del cliente como cotizaciones, facturaciones, pagos de servicios, un amplio registro de transportes y conductores, relación de viajes y cargas de cada uno de ellos además de sus respectivas ganancias y pagos. El sistema permanece en funcionamiento y uso aunque el sitio ya no se encuentra disponible online.
+			</div>
+
+		</section>
+		<br>
+		<section id="contpage">
+
+			<div id="contpageimg">
+				<img src="img/page/5.png">
+			</div>
+			<div id="contpagetext">
+				<h3>The Mister Book</h3>
+				En éste proyecto se planea crear una comunidad de usuarios donde estos podrán cargar y descargar libros de forma gratuita. En un principio consistirá en libros de programación y desarrollo, sin embargo también se motivará a los usuarios a compartir su información relacionada a otras áreas.
+			</div>
+
+		</section>
+		<br>
+		<section id="contpage">
+
+			<div id="contpageimg">
+				<img src="img/page/6.png">
+			</div>
+			<div id="contpagetext">
+				<h3>Sistema de inspecciones</h3>
+				Este sistema es para el Ejercito Bolivariano (hombres y mujeres valientes que luchan por el bienestar de la nación, para defender nuestros ideales de democracia y promover el bienestar social). en este proyecto se registra el personal militar del ejercito, ademas se registran las empresas, luego se asigna determinado personal a determinada empresa para realizar las inspecciones, ademas se debe imprimir la carta y formulario que se llenara al llegar a dicha empresa, ademas se mostraran los reportes segun fecha, empresa o personal.
+			</div>
+
+		</section>
+		<br>
+		<section id="contpage">
+
+			<div id="contpageimg">
+				<img src="img/page/7.png">
+			</div>
+			<div id="contpagetext">
+				<h3>SADN</h3>
+				El “Sistema Administrativo Divino Niño” fue desarrollado para la tesis universitaria de uno de nuestros clientes. Cuenta con una variedad de funciones de dicha índole como el registro tanto de alumnos como profesores, la creación de una “cédula escolar” para cada sección, un módulo de “documentos” donde se realizan solicitudes de archivos como cartas de buena conducta, de promoción, referencias laborales, entre otros, de forma clara y sencilla eligiendo al alumno o profesor y generando automáticamente el documento en formato PDF. El sistema se actualiza cada año escolar ya que las secciones varían.
+			</div>
+
+		</section>
+		<br>
+		<section id="contpage">
+
+			<div id="contpageimg">
+				<img src="img/page/8.png">
+			</div>
+			<div id="contpagetext">
+				<h3>Sistema de archivo</h3>
+				Un eficiente sistema creado para el Ejército Bolivariano de Venezuela. Su principal función es el registro de los APOA (Anteproyecto de Plan Operativo Anual) y los POA (Plan Operativo Anual) de las 377 unidades militares distribuidas en todo el territorio nacional. El programa también posee otras funciones importantes ya que muestra cuántas de las unidades han entregado el proyecto, cuántas faltan por hacerlo, y brinda la opción de acceder a cualquiera de ellas de manera sencilla y efectiva mediante sólo un clic.
+			</div>
+
+		</section>
+		<br>
+		<section id="contpage">
+
+			<div id="contpageimg">
+				<img src="img/page/9.png">
+			</div>
+			<div id="contpagetext">
+				<h3>Hotel PuntoFino</h3>
+				Esta es una pagina para un proyecto de un estudiante, se muestra las distintas habitaciones y promociones del hotel, ademas de una seccion de contacto. cuenta ademas con un sistema administrativo donde se pueden registrar los clientes y luego mostrar los gastos de los mismos, ademas mostrar un reporte de ingresos por fecha
+			</div>
+
+		</section>
+		<br>
+		<section id="contpage">
+
+			<div id="contpageimg">
+				<img src="img/page/10.png">
+			</div>
+			<div id="contpagetext">
+				<h3>SOAS</h3>
+				Sitio web y sistema creados con fines académicos, donde los profesores pueden visualizar próximos eventos o realizar consultas (como quejas y demás solicitudes) a su sindicato correspondiente.
+			</div>
+
+		</section>
+		<br>
+		<section id="contpage">
+
+			<div id="contpageimg">
+				<img src="img/page/11.png">
+			</div>
+			<div id="contpagetext">
+				<h3>Creatilia - let's go</h3>
+				Bueno esta pagina estuvo online un tiempo, fue la pagina predecesora a Edosistems que es predecesora de e2sistems, es solo la pagina de la empresa, como esta tiene inicio, acerca, contactanos, el equipo, blog, servicios... etc.
+			</div>
+
+		</section>
+		<br>
+		<section id="contpage">
+
+			<div id="contpageimg">
+				<img src="img/page/12.png">
+			</div>
+			<div id="contpagetext">
+				<h3>DAEX</h3>
+				Este sistema es para el Ejercito Bolivariano (hombres y mujeres valientes que luchan por el bienestar de la nación, para defender nuestros ideales de democracia y promover el bienestar social) Copy Paste . este sistema es especificamente para el Departamento de Armas y Explosivos, el sistema permite al usuario registrarse, ingresar luego sus datos personales y registrar cada una de sus armas, luego el sistema asignara una cita, la cual enviara por correo, con un archivo PDF adjunto en donde se mostrara el identificador, sus datos y las distintas armas, el mismo debe ser llevado el dia de la cita para realizar el registro de las armas de forma oficial (vamos que como el saime pero en lugar de pasaporte, armas).... el sistema cuenta con una puerta trasera (no creo que sea buena idea decir eso) en esta puerta trasera solo pueden accerder administradores, para registrar a sus "amigos" y adelantarle el tramite  (trampas, siempre trampas, luego dicen que xq esos tramiten se demoran y son tan problematicos, pero en fin como dije al principio, estos valientes defienden nuestros intereses y de nuestra nacion)
+			</div>
+
+		</section>
+		<br>
 
 
 	</div>
